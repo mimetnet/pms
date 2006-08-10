6c096a1bba30a421ef5ee1be0a3d1a1d99944a1d
Matthew Metnetsky <matthew@cowarthill.com>
2006-08-10 10:04:29 -0400

    Upgraded version to 0.6.33.0

--- PMS.NUnit/Properties/AssemblyInfo.cs
+++ PMS.NUnit/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.6.32.0")]
-[assembly: AssemblyFileVersion("0.6.32.0")]
+[assembly: AssemblyVersion("0.6.34.0")]
+[assembly: AssemblyFileVersion("0.6.34.0")]
