0037717d6d4c8bfc02b2f3225867d72db192a7e0
Matthew Metnetsky <matthew@cowarthill.com>
2006-08-15 09:45:42 -0400

    - added PMS build via make
     - TODO: add PMS.NUnit & PMS.NUnit.Model to make

---
+++ build/rules.mk
@@ -0,0 +1,70 @@
+# Author: Matthew Metnetsky <matthew@cowarthill.com>
+#
+# Useful link -> http://dev.panopticsearch.com/make-notes.html
+##############################################################
+
+# if not linux
+ifdef COMSPEC
+	TR = '\\\\'
+	CSC = /cygdrive/c/WINDOWS/Microsoft.NET/Framework/v2.0/csc.exe
+else
+	TR = '/'
+	CSC = gmcs
+	EXE = mono
+endif
+
+##############################################################################
+# Variables
+############
+
+build = bin
+deps = $(build)/deps
+
+ass.bin = $(build)/$(ass)
+ass.sources = $(deps)/$(ass).sources
+ass.make = $(deps)/$(ass).make
+
+# override for distribution compilation (no console logging in a release!!)
+target = library
+ifdef TARGET
+	target = $(TARGET)
+endif
+
+
+##############################################################################
+# Begin Rules
+##############
+
+$(ass.bin): $(build) $(ass.sources)
+	$(CSC) -d:NET_2_0 /target:$(target) /out:$@ $(LIBRARIES) @$(ass.sources)
+
+# Build ass.sources
+ifneq ($(ass.sources),$(ass.stamp))
+$(ass.sources): $(deps)
+	@ls */*.cs | uniq -u | tr '/' '$(TR)' > $@
+endif
+
+# Build make frag which forces all *.cs files to be depencies of CMN.Util.dll
+$(ass.make): $(ass.sources)
+	@sed 's,^,$(ass.bin): ,' $< >$@
+
+-include $(ass.make)
+
+# create build directory and its children if its not there
+$(build):
+	@mkdir -p $@
+
+$(deps):
+	@if test ! -d $@; then mkdir $@; fi;
+
+# record out latest build
+$(ass.stamp):
+	@touch $@
+
+clean:
+	rm -rf $(ass.bin) $(deps)
+
+distclean: clean
+
+.PHONY: run dist clean distclean
+
