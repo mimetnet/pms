bdb6ac28d6f6f14e175bfcc0818805288de731ef
Matthew Metnetsky <matthew@cowarthill.com>
2010-01-22 00:14:32 -0500

    Field - ToString() now trims trailing spaces

--- PMS/PMS.Metadata/Field.cs
+++ PMS/PMS.Metadata/Field.cs
@@ -122,13 +122,11 @@ namespace PMS.Metadata
 
         public override string ToString()
         {
-            //return String.Format("[ Field (Name={0}) (Column={1}) (DbType={2}) (PrimaryKey={3}) (IgnoreDefault={4}) ]", Name, Column, DbType, PrimaryKey, IgnoreDefault);
-
             return String.Format("{0} {1} {2} {3}", //{4} == NOT NULL
                 this.Column,
                 this.DbType,
                 (this.PrimaryKey) ? "PRIMARY KEY" : String.Empty,
-                (this.Unique && !this.PrimaryKey) ? "UNIQUE" : String.Empty);
+                (this.Unique && !this.PrimaryKey) ? "UNIQUE" : String.Empty).Trim();
         }
 
         public override int GetHashCode()
